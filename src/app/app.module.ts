830e1bdff6841a9bc3f3926fc97122d94efaca36
codroidgotham <no.one1970@protonmail.com>
2023-01-17 22:45:00 +0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
 import { AppComponent } from './app.component';
-
+import {MatSidenavModule} from '@angular/material/sidenav'
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatButtonModule} from '@angular/material/button'
+import {MatMenuModule} from '@angular/material/menu'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,BrowserAnimationsModule,MatIconModule,MatListModule,MatSidenavModule,MatToolbarModule,MatButtonModule,MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
