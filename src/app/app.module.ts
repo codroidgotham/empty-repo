18f42296347cab2661f24ee03b18069955cc6907
codroidgotham <no.one1970@protonmail.com>
2023-01-31 16:33:26 +0500

    final push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,6 +36,8 @@ import {MatGridListModule} from '@angular/material/grid-list';
 import { AddThreatDialogComponent } from './add-threat-dialog/add-threat-dialog.component';
 import { AddVulnerabilityDialogComponent } from './add-vulnerability-dialog/add-vulnerability-dialog.component';
 import { AddControlDialogComponent } from './add-control-dialog/add-control-dialog.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 const routes: Routes = [{ path: "", component: AboutComponent },{path:"RiskAssessment",component:PerformComponent,resolve:{data:ClassResolver}},{path:"login",component:LoginComponent}, { path: "classes", component: ClassesComponent,resolve:{data:ClassResolver} }, { path: "**", component: AboutComponent }]
 @NgModule({
   declarations: [
@@ -57,7 +59,7 @@ const routes: Routes = [{ path: "", component: AboutComponent },{path:"RiskAsses
   imports: [
     BrowserModule, BrowserAnimationsModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule, MatButtonModule, MatMenuModule, RouterModule.forRoot(routes), HttpClientModule
     , MatCardModule, MatTableModule, MatDialogModule, FormsModule, MatFormFieldModule, MatSelectModule, MatDatepickerModule, ReactiveFormsModule, MatNativeDateModule,MatInputModule
-  ,MatProgressSpinnerModule,MatChipsModule,MatGridListModule
+  ,MatProgressSpinnerModule,MatChipsModule,MatGridListModule,FlexLayoutModule,MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/perform/perform.component.ts
+++ src/app/perform/perform.component.ts
@@ -1,9 +1,10 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-import { map, Observable, of } from 'rxjs';
+import { first, map, Observable, of } from 'rxjs';
 import { Class } from '../Models/Class';
 import { MatDialogConfig, MatDialog } from '@angular/material/dialog';
+import { MatSnackBarConfig,MatSnackBar } from '@angular/material/snack-bar';
 import { ClassDialogComponent } from '../class-dialog/class-dialog.component';
 import { FetchDataService } from '../fetch-data.service';
 import { MatSelectionList } from '@angular/material/list';
@@ -29,7 +30,7 @@ export class PerformComponent implements OnInit, AfterViewInit {
   displayedColumns = ['threat', 'Impact', 'Controls', 'Vulnerabilities', 'Likelihood', 'add']
 
   form: FormGroup
-  constructor(private fb: FormBuilder, private route: ActivatedRoute, private dialog: MatDialog, private dataService: FetchDataService) {
+  constructor(private fb: FormBuilder, private route: ActivatedRoute, private dialog: MatDialog, private dataService: FetchDataService,private snack:MatSnackBar) {
 
 
 
@@ -107,23 +108,28 @@ export class PerformComponent implements OnInit, AfterViewInit {
   }
 
   addRiskRow() {
-
+    let tmp1;
     const obj = { "ClassName":(this.form.controls['Classe'].value.Name),"Threat": (this.form.controls['Threat'].value.statement), "Impact":this.form.controls['Impact'].value ,
     "Controls":this.a.selectedOptions.selected.map(o=>o.value.statement),"Vulnerabilities":this.b.selectedOptions.selected.map(o=>o.value.statement),"Likelihood": this.form.controls['Likelihood'].value,"Risk":'H' };
     if (this.form.valid)
     {
-      this.dataService.insertRiskRow(obj).subscribe(
+      this.dataService.insertRiskRow(obj).pipe(first()).subscribe(
         val=>{
+          console.log("value",val);
+          tmp1=val;
+          console.log("just assigned",tmp1);
           if (val){
              this.partialReset();
-            
+            this.opensnackbar("row successfully inserted!")
+          }else{
+            this.opensnackbar("row could not be inserted!")
           }
         }
 
       )
     }
 
-    console.log(this.b.selectedOptions.selected.map(o=>o.value.statement))
+    
   }
   addThreat() { }
 
@@ -139,6 +145,12 @@ export class PerformComponent implements OnInit, AfterViewInit {
       
       this.Vulnerabilities$ = of(null);this.Controls$=of(null); this.form.controls["Likelihood"].reset(); this.Threats$ = this.dataService.fetchAllThreats();
       // this.form.clearValidators()
+  }
+
+  opensnackbar(message){
+    this.snack.open(message,"");
+    setTimeout(()=>this.snack.dismiss(),3000)
+
   }
   
 
